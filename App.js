21f9cdb84ce4b8ab24e2d65c5ddbb14412c773b7
Guilherme Sousa <guilherme.gs17@outlook.com>
2023-03-26 10:35:08 -0300

    Ajustando os parâmetros necessários da tela CalculateScreen, passando as configurações corretas para a tela

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import InitialScreen from './src/screens/InitialScreen/index'
 import WelcomeScreen from './src/screens/WelcomeScreen/index'
 import CalculateScreen from './src/screens/CalculateScreen/index'
+import ResultantScreen from './src/screens/ResultantScreen/index'
 import { NavigationContainer } from '@react-navigation/native'
 
 const Stack = createNativeStackNavigator()
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="InitialScreen" component={InitialScreen} />
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="CalculateScreen" component={CalculateScreen} />
+        <Stack.Screen name="ResultantScreen" component={ResultantScreen} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
